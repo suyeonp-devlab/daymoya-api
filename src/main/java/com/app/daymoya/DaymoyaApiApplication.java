e34b35dcc72b9bda517ca94039a833027da524e1
suyeon <suyeondevlab@gmail.com>
2026-04-28 23:47:42 +0900

    chore: 프로젝트 init

---
+++ src/main/java/com/app/daymoya/DaymoyaApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.daymoya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaymoyaApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DaymoyaApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/app/daymoya/DaymoyaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.daymoya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaymoyaApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
